82349357995555fa4b1c08cdd3db300421a5d073
Vitória Oliveira <mvitoriacoliveira@gmail.com>
2024-11-14 18:21:26 +0100

    add fragments notes, main activity class + main activity and notes fragments layout

---
+++ daabasictemplate/app/src/main/java/ch/heigvd/iict/daa/template/FragmentNotes.kt
@@ -0,0 +1,19 @@
+package ch.heigvd.iict.daa.template
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentNotes : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.notes_fragment, container, false)
+    }
+}
--- daabasictemplate/app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
+++ daabasictemplate/app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
@@ -3,9 +3,53 @@ package ch.heigvd.iict.daa.template
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, FragmentNotes())
+                .commit()
+        }
+
     }
+
+    // CHECK FUNCTIONS BELOW
+    /*
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu defined in res/menu/main_menu.xml
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle menu actions based on the item selected
+        return when (item.itemId) {
+            R.id.menu_sort_by_creation_date -> {
+                // Action to sort notes by creation date
+                // TODO: Implement sorting by creation date here
+                true
+            }
+            R.id.menu_sort_by_schedule_date -> {
+                // Action to sort notes by scheduled date
+                // TODO: Implement sorting by scheduled date here
+                true
+            }
+            R.id.menu_generate_note -> {
+                // Action to generate a random note
+                // TODO: Implement note generation logic here
+                true
+            }
+            R.id.menu_delete_all_notes -> {
+                // Action to delete all notes
+                // TODO: Implement logic to delete all notes here
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+     */
 }
