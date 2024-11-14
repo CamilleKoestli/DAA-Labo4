3a76cc713a3d8b2476c3f0f3076dc97e9d3acf58
CamilleKoestli <91135499+CamilleKoestli@users.noreply.github.com>
2024-11-14 18:13:12 +0100

    feat: repository

---
+++ daabasictemplate/app/src/main/java/ch/heigvd/iict/daa/template/repositories/DataRepository.kt
@@ -0,0 +1,34 @@
+import androidx.lifecycle.LiveData
+import ch.heigvd.iict.daa.labo4.models.Note
+import ch.heigvd.iict.daa.labo4.models.NoteAndSchedule
+import ch.heigvd.iict.daa.labo4.models.Schedule
+import ch.heigvd.iict.daa.template.Dao.NoteDao
+import ch.heigvd.iict.daa.template.Dao.ScheduleDao
+
+class DataRepository(private val noteDao: NoteDao, private val scheduleDao: ScheduleDao) {
+
+    val allNotes: LiveData<List<NoteAndSchedule>> = noteDao.getAllNotes()
+    val countNotes: LiveData<Int> = noteDao.countNotes()
+
+    suspend fun insert(note: Note) {
+        noteDao.insert(note)
+    }
+
+    suspend fun insert(schedule: Schedule) {
+        scheduleDao.insert(schedule)
+    }
+
+    suspend fun delete(note: Note) {
+        noteDao.delete(note)
+    }
+
+    suspend fun deleteAllNotes() {
+        noteDao.deleteAllNotes()
+    }
+
+    // Méthode pour générer une note aléatoire et l'insérer dans la base
+    suspend fun generateRandomNote() {
+        val randomNote = Note.generateRandomNote()
+        noteDao.insert(randomNote)
+    }
+}
--- daabasictemplate/app/src/main/java/ch/heigvd/iict/daa/template/repositories/Repository.kt
+++
@@ -1,4 +0,0 @@
-package ch.heigvd.iict.daa.template.repositories
-
-class Repository {
-}
